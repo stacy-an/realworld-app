d3f0ee4f5deffb56f5eec040fa061305f985641b
Anastasia Anufriev <stacy.an.v@gmail.com>
2024-02-11 19:54:47 +0200

    add tests account-details

---
+++ playwright/tests/e2e/account-details.spec.mjs
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../../pages/HomePage.mjs";
+import { LoginPage } from "../../pages/LoginPage.mjs";
+import { validUser } from "../../test-data/users.mjs";
+
+test.beforeEach(async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.userLogin(validUser.username, validUser.password);
+  await loginPage.verifyErrorIsDisplayed(false);
+});
+
+test("should see account details", async ({ page }) => {
+  const homePage = new HomePage(page);
+  await homePage.verifyAccountBalanceIsVisible();
+});
