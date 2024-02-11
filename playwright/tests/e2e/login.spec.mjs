e72df40cc5b7f933f4f50ac7eba4dad85348262d
Anastasia Anufriev <stacy.an.v@gmail.com>
2024-02-11 20:57:21 +0200

    apply changes by prettier

--- playwright/tests/e2e/login.spec.mjs
+++ playwright/tests/e2e/login.spec.mjs
@@ -1,15 +1,15 @@
-import { test, expect } from "@playwright/test";
-import { LoginPage } from "../../pages/LoginPage.mjs";
-import { newRandomUser, validUser } from "../../test-data/users.mjs";
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage.mjs';
+import { newRandomUser, validUser } from '../../test-data/users.mjs';
 
-test("should log in", async ({ page }) => {
+test('should log in', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
   await loginPage.userLogin(validUser.username, validUser.password);
   await loginPage.verifyErrorIsDisplayed(false);
 });
 
-test("should not login with non existed user", async ({ page }) => {
+test('should not login with non existed user', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
   await loginPage.userLogin(newRandomUser.username, newRandomUser.password);
