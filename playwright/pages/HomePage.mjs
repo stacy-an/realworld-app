a698f0e2fd1d369c41db900f5d59ec935fa0f3e5
Anastasia Anufriev <stacy.an.v@gmail.com>
2024-02-11 20:56:15 +0200

    add new method to HomePage

--- playwright/pages/HomePage.mjs
+++ playwright/pages/HomePage.mjs
@@ -1,23 +1,21 @@
 import { expect } from '@playwright/test';
+
 export class HomePage {
+  constructor(page) {
+    this.page = page;
+    this.accountBalance = page.locator('[data-test="sidenav-user-balance"]');
+    this.userFullName = page.locator('[data-test="sidenav-user-full-name"]');
+    this.accountUsername = page.locator('[data-test="sidenav-username"]');
+    this.accountButton = page.locator("a[data-test='sidenav-bankaccounts']");
+  }
 
-    constructor(page) {
-        this.page = page;
-        this.accountBalance = page.locator('[data-test="sidenav-user-balance"]');
-        this.userFullName = page.locator('[data-test="sidenav-user-full-name"]');
-        this.accountUsername = page.locator('[data-test="sidenav-username"]');
-        this.accountButton = page.locator("a[data-test='sidenav-bankaccounts']")
-    }
+  async verifyAccountBalanceIsVisible() {
+    await expect(this.accountBalance).toBeVisible();
+    await expect(this.accountBalance).toContainText('$');
+  }
 
-    async verifyAccountBalanceIsVisible(isVisible){
-        if (isVisible == true){
-            await expect(this.accountBalance).toBeVisible();
-            await expect(this.accountBalance).toHaveText('$');
-          }
-          else {
-            await expect(this.accountBalance).toHaveCount(0);
-          } 
-        
-    }
-
+  async verifyUserFullNameIsVisible(firstName, lastName) {
+    await expect(this.userFullName).toBeVisible();
+    await expect(this.userFullName).toHaveText(firstName + ' ' + lastName.charAt(0));
+  }
 }
