648c5f933e21e467b47ab7d4592f40a71bc4fffc
Anastasia Anufriev <stacy.an.v@gmail.com>
2024-02-15 17:34:58 +0200

    add new test user

--- playwright/test-data/users.mjs
+++ playwright/test-data/users.mjs
@@ -1,16 +1,16 @@
-import { generateString } from '../helpers/data-helper.mjs';
+import { generateString } from "../helpers/data-helper.mjs";
 
 export const validUser = {
-  id: 't45AiwidW',
-  uuid: '6383f84e-b511-44c5-a835-3ece1d781fa8',
-  firstName: 'Edgar',
-  lastName: 'Johns',
-  username: 'Katharina_Bernier',
-  password: 's3cret',
-  email: 'Norene39@yahoo.com',
-  phoneNumber: '625-316-9882',
-  avatar: 'https://cypress-realworld-app-svgs.s3.amazonaws.com/t45AiwidW.svg',
-  defaultPrivacyLevel: 'public',
+  id: "t45AiwidW",
+  uuid: "6383f84e-b511-44c5-a835-3ece1d781fa8",
+  firstName: "Edgar",
+  lastName: "Johns",
+  username: "Katharina_Bernier",
+  password: "s3cret",
+  email: "Norene39@yahoo.com",
+  phoneNumber: "625-316-9882",
+  avatar: "https://cypress-realworld-app-svgs.s3.amazonaws.com/t45AiwidW.svg",
+  defaultPrivacyLevel: "public",
   balance: 168137,
 };
 
@@ -19,4 +19,11 @@ export const newRandomUser = {
   lastName: generateString(7),
   username: generateString(7),
   password: generateString(7),
+};
+
+export const updatedUser = {
+  firstName: "Kate",
+  lastName: "Lo",
+  email: "afterupdate@yahoo.com",
+  phoneNumber: "625-316-1111",
 };
