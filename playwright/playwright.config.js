bd9eda4867a53ccd74576bf144ac4d485a907222
Anastasia Anufriev <stacy.an.v@gmail.com>
2024-03-03 21:24:27 +0200

    add testIdAttribute to config file

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -34,6 +34,7 @@ const config = {
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    testIdAttribute: "data-test",
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     headless: false,
